f1d40429be741e983f3097ed8516cd2073f58413
Arya Krishna <dr.aryakris@outlook.com>
2022-03-17 23:46:28 -0700

    working with thought controllers

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/postsTags', {
+connect('mongodb://mongodb://127.0.0.1:27017/postsTags', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
